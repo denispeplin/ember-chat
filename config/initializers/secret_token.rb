82a583d56f1f2c117f4ad63c114e60ae11f3fad9
denispeplin <denis.peplin@gmail.com>
2014-02-12 21:52:13 +0400

    New Rails application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberChat::Application.config.secret_key_base = '223d352b062e8f383646383ff088878ec6d34c3973cc3ba255815d510b2cc8eb948b4fb40b176550d0c8c79a07bcd7027f51e4b423460c9b1b206ae121c2ce42'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmberChat::Application.config.session_store :cookie_store, key: '_ember-chat_session'
