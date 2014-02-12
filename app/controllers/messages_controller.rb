c5228f4c99b71cc30cee7993f49bbfbcff9351f4
denispeplin <denis.peplin@gmail.com>
2014-02-12 22:09:50 +0400

    Implement 'index' and 'create' actions for messages.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  def index
+    @messages = Message.all
+    render json: @messages
+  end
+
+  def create
+    @message = Message.new params.require(:message).permit(:body)
+    if @message.save
+      render json: @message
+    end
+  end
 end
